6773f6bb363fba6a48281460302482947daee9b3
mdmoreau <mdmoreau@gmail.com>
2018-08-31 01:09:11 -0400

    initial setup

---
+++ wp-config-local.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'mysql');
+define('WP_HOME', 'http://localhost');
+define('WP_SITEURL', 'http://localhost');
