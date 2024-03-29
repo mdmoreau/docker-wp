d8d027d914a509255cadcc4f94ea4fc4a1e982e9
mdmoreau <mdmoreau@gmail.com>
2024-03-29 19:27:52 -0400

    update config

--- wp-config-local.php
+++ wp-config-local.php
@@ -1,9 +1,11 @@
 <?php
+define( 'DB_HOST', 'database' );
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'username' );
 define( 'DB_PASSWORD', 'password' );
-define( 'DB_HOST', 'database' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+define( 'WP_DEBUG', true );
 define( 'WP_HOME', 'http://' . ($_SERVER['HTTP_HOST'] ?? 'localhost') );
 define( 'WP_SITEURL', 'http://' . ($_SERVER['HTTP_HOST'] ?? 'localhost') );
+define( 'WP_ENVIRONMENT_TYPE', 'local' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
