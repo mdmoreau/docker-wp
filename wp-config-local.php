7883ffa50346d56d766c7a6875f0118c6b5cda1f
mdmoreau <mdmoreau@gmail.com>
2024-03-31 22:41:36 -0400

    remove unnecessary config

--- wp-config-local.php
+++ wp-config-local.php
@@ -6,6 +6,3 @@ define( 'DB_PASSWORD', 'password' );
 define( 'WP_DEBUG', true );
 define( 'WP_HOME', 'http://' . ($_SERVER['HTTP_HOST'] ?? 'localhost') );
 define( 'WP_SITEURL', 'http://' . ($_SERVER['HTTP_HOST'] ?? 'localhost') );
-define( 'WP_ENVIRONMENT_TYPE', 'local' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
