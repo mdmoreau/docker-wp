058d8bc66fd61ed77df21a3b2d629850736c518d
mdmoreau <mdmoreau@gmail.com>
2025-11-10 12:36:44 -0500

    add env, dev config

--- wp-config-local.php
+++ wp-config-local.php
@@ -4,5 +4,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'username' );
 define( 'DB_PASSWORD', 'password' );
 define( 'WP_DEBUG', true );
+define( 'WP_ENVIRONMENT_TYPE', 'local' );
+define( 'WP_DEVELOPMENT_MODE', 'all' );
 define( 'WP_HOME', 'http://' . ($_SERVER['HTTP_HOST'] ?? 'localhost') );
 define( 'WP_SITEURL', 'http://' . ($_SERVER['HTTP_HOST'] ?? 'localhost') );
